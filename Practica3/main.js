defb0f292c002dd65581c55c21b85a5025d5d95f
SeleLira-20 <liraselene26@gmail.com>
2025-09-25 17:42:37 -0600

    Ejercicio 2 práctica 3

--- Practica3/main.js
+++ Practica3/main.js
@@ -7,3 +7,24 @@ console.log("10-5 =", restar(10, 5));
 console.log("20-8 =", restar(20, 8));
 console.log("7-3 =", restar(7, 3));
 console.log("100-25 =", restar(100, 25));
+
+// verrificar usuario
+
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+verificarUsuario("admin")
+  .then(res => console.log(res))   // Aparece: Acceso concedido
+  .catch(err => console.error(err));
+
+verificarUsuario("Ivan")
+  .then(res => console.log(res))   //  Acceso denegado
+  .catch(err => console.error(err));
+
