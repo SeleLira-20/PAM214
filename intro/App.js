d5b21237a336e18367f39fe49c0a18e07a22fb7a
SeleLira-20 <liraselene26@gmail.com>
2025-10-09 17:47:44 -0600

    Creacion de botones

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,22 @@
+//1.imports:Zona de las importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+//2. main: Zona de componentes 
+export default function App() { //aqui podemos ver la vista de la app
+  const[Contador, setContador]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola mundo react native</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Contador: {Contador}</Text>
+      <Button title='Agregar' onPress={()=>setContador (Contador+1)}/>
+      <Button title='Quitar' onPress={()=>setContador (Contador-1)}/>
+      <Button title='Reiniciar' onPress={()=>setContador (Contador - Contador)}/>
+      <StatusBar style="auto" /> 
+    </View> // para que el componente sea visible simpre debe estar dentro de un View 
   );
 }
 
+//3. Estilos: Zona estetica y posicionamiento 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
