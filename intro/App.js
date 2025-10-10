f33f68e590278e096b54b2a136977eb84f08e219
SeleLira-20 <liraselene26@gmail.com>
2025-10-10 15:37:01 -0600

    Practica 6

--- intro/App.js
+++ intro/App.js
@@ -1,27 +1,43 @@
 //1.imports:Zona de las importaciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
+import ContadorSreen from './screens/ContadorScreen'
 //2. main: Zona de componentes 
-export default function App() { //aqui podemos ver la vista de la app
-  const[Contador, setContador]=useState(0);
+export default function App() { 
+  <ContadorSreen></ContadorSreen>
   return (
-    <View style={styles.container}>
-      <Text>Contador: {Contador}</Text>
-      <Button title='Agregar' onPress={()=>setContador (Contador+1)}/>
-      <Button title='Quitar' onPress={()=>setContador (Contador-1)}/>
-      <Button title='Reiniciar' onPress={()=>setContador (Contador - Contador)}/>
-      <StatusBar style="auto" /> 
-    </View> // para que el componente sea visible simpre debe estar dentro de un View 
+    
   );
 }
 
 //3. Estilos: Zona estetica y posicionamiento 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //asigna los espacios para la vista y pocisionamineto 
+    backgroundColor: '#cb396cff', //colores (instalar la extencion color bi)
+    alignItems: 'center', // posionar lo que ves en la pantalla derecha, iszaquierda eje x
+    justifyContent: 'center', //posicionar lo que vemos en la pantalla arriba o abajo, eje y 
+    // star (arriba, izaquierda), end (abajo, derecha)
+  }, // importante colocar la coma 
+
+  texto:{
+    color:'#370fe8ff', //color del texto
+    fontSize:30,
+    fontFamily: 'Time New Roman', // cambiar la letra 
+    fontWeight: 'bold', //negrita
+    fontStyle: 'italic',//se haga de lado 
+    textDecorationLine: 'line-through',
+
   },
-});
+  texto2:{
+    color:'#4bbeefff', //color del texto
+    fontSize:40,
+    fontFamily: 'Courier', // cambiar la letra 
+    fontWeight: '900', 
+    textDecorationLine: 'unerLine',
+  },
+
+  contenedorBotones:{
+    marginTop: 15,
+    flexDirection: 'row',
+    gap:15
+  }
+  });
